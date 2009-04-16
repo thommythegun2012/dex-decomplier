a497097ad55c36e94e0f2080dec334d9cb0a68b6
wendal1985 <wendal1985@3753397e-21df-11de-a78b-a7aca27b2395>
2009-04-16 05:20:33 +0000

    Fix move的处理

--- trunk/src/com/wendal/java/dex/decomplier/javafile/model/statement/PrototypeStatement_Move.java
+++ trunk/src/com/wendal/java/dex/decomplier/javafile/model/statement/PrototypeStatement_Move.java
@@ -1,6 +1,7 @@
 package com.wendal.java.dex.decomplier.javafile.model.statement;
 
 import com.wendal.java.dex.decomplier.javafile.model.PrototypeStatement;
+import com.wendal.java.dex.decomplier.toolkit.String_Toolkit;
 
 public class PrototypeStatement_Move extends PrototypeStatement {
     
@@ -24,6 +25,13 @@ public class PrototypeStatement_Move extends PrototypeStatement {
         
         if(info.indexOf("-object ") > -1){
             isObject = true;
+            if(var_b_name.startsWith("L")){
+                String class_name_temp = var_b_name.substring(0, var_b_name.indexOf(";"));
+                String class_name =String_Toolkit.parseSingleClassName(class_name_temp);
+                String var_name = var_b_name.substring(var_b_name.indexOf(".")+1,var_b_name.indexOf(":"));
+                
+                var_b_name = class_name + "." + var_name;
+            }
         }
     }
 

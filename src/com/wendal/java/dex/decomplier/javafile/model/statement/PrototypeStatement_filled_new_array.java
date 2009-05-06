c0d3a8cfec7da03452dee5d5219e0671635935d6
wendal1985 <wendal1985@3753397e-21df-11de-a78b-a7aca27b2395>
2009-05-06 06:47:19 +0000

    support --> filled_new_array filled_new_array_range

---
+++ src/com/wendal/java/dex/decomplier/javafile/model/statement/PrototypeStatement_filled_new_array.java
@@ -0,0 +1,32 @@
+package com.wendal.java.dex.decomplier.javafile.model.statement;
+
+import com.wendal.java.dex.decomplier.javafile.model.PrototypeStatement;
+import com.wendal.java.dex.decomplier.toolkit.String_Toolkit;
+
+public class PrototypeStatement_filled_new_array extends PrototypeStatement {
+public String [] v_data;
+    
+    public String vx_type;
+
+    @Override
+    public void parse() {
+
+        super.parse();
+
+        v_data = info.substring(info.indexOf("{")+1 , info.indexOf("}")).split(",");
+        for (int i = 0; i < v_data.length; i++) {
+            v_data[i] = v_data[i].trim();
+        }
+
+        vx_type = String_Toolkit.parseType(info.substring(info.indexOf("[")+1).trim());
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "";
+        for (int i = 0; i < v_data.length; i++) {
+            tmp = tmp + " ," + v_data[i];
+        }
+        return super.toString() + "\n new " + vx_type + " {"+ tmp +"} /*";
+    }
+}

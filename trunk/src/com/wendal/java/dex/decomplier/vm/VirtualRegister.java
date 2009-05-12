f5ee09ea11d23dde99aaeb22851e3fdaea4f74e1
wendal1985 <wendal1985@3753397e-21df-11de-a78b-a7aca27b2395>
2009-05-12 05:25:45 +0000

    Start try to parse simple method , without goto if switch

--- trunk/src/com/wendal/java/dex/decomplier/vm/VirtualRegister.java
+++ trunk/src/com/wendal/java/dex/decomplier/vm/VirtualRegister.java
@@ -1,7 +1,10 @@
 package com.wendal.java.dex.decomplier.vm;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.wendal.java.dex.decomplier.dexfile.model.Dex_Method.LocalVar;
 
 public class VirtualRegister {
     
@@ -18,5 +21,18 @@ public class VirtualRegister {
 
     public void putRegiter(String vx , RegisterUnit ru){
         maps.put(vx, ru);
+    }
+    
+    public void initLocalVa(List<LocalVar> lv){
+        for (LocalVar localVar : lv) {
+            String r_name = "v"+localVar.reg;
+            String r_val = localVar.name;
+            
+            RegisterUnit ru = new RegisterUnit();
+            ru.setType(RegisterUnit.LV);
+            ru.setValue(r_val);
+            
+            maps.put(r_name, ru);
+        }
     }
 }

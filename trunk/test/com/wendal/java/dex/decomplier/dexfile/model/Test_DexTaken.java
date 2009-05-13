6079b86d9bb524abbb95ef2352158f386b7a2489
wendal1985 <wendal1985@3753397e-21df-11de-a78b-a7aca27b2395>
2009-05-13 06:38:19 +0000

    Update test method

--- trunk/test/com/wendal/java/dex/decomplier/dexfile/model/Test_DexTaken.java
+++ trunk/test/com/wendal/java/dex/decomplier/dexfile/model/Test_DexTaken.java
@@ -3,6 +3,7 @@ package com.wendal.java.dex.decomplier.dexfile.model;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,27 +16,36 @@ public class Test_DexTaken {
 
     @Test
     public void testGetDexClassList() throws IOException {
-        String fileList [] = new String[]{
-                "dex/com/wendal/dex/simple/multi/xrace_v2.dump.txt"
-                ,
-                "dex/com/wendal/dex/simple/multi/rec.dump.txt"
-                , "dex/com/wendal/dex/simple/empty/EmptyClass.dump.txt"
-                ,"dex/com/wendal/dex/simple/empty/EmptyInterface.dump.txt"
-                ,"dex/com/wendal/dex/simple/empty/EmptyEnum.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_Abstract.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_fields.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_final.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_SomeInterfaces.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_Static_fields.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/SimpleClass_with_SuperClass.dump.txt"
-                ,"dex/com/wendal/dex/simple/easy/methods/Void_String_Method.dump.txt"
-                ,
-                "dex/com/wendal/dex/simple/easy/methods/Static_Methods.dump.txt"
-        };
-        for (String string : fileList) {
+        List<String> file_list = new ArrayList<String>();
+        file_list.add("dex/com/wendal/dex/simple/multi/xrace_v2.dump.txt");
+        file_list.add("dex/com/wendal/dex/simple/multi/rec.dump.txt");
+        file_list.add("dex/com/wendal/dex/simple/empty/EmptyClass.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/empty/EmptyInterface.dump.txt");
+        file_list.add("dex/com/wendal/dex/simple/empty/EmptyEnum.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_Abstract.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_fields.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_final.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_SomeInterfaces.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_Static_fields.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/SimpleClass_with_SuperClass.dump.txt");
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/methods/Void_String_Method.dump.txt");
+
+        file_list
+                .add("dex/com/wendal/dex/simple/easy/methods/Static_Methods.dump.txt");
+        
+        for (String string : file_list) {
             List<String> list = IO_Tool.getFile(string);
             assertTrue(list.size() > 0);
-            DexD dexD = new DexD(CommandLineConfig.parse(new String[]{ "-dest" ,"X:"}));
+            DexD dexD = new DexD(CommandLineConfig.parse(new String[] {
+                    "-dest", "X:" }));
             dexD.setDex_dump_str(list);
             dexD.convert2DexModel();
             dexD.convert2JavaModel();
@@ -43,7 +53,7 @@ public class Test_DexTaken {
             dexD.reshaping();
             dexD.outputSource();
         }
-        
+
     }
 
 }
